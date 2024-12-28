fa8e5ba660a1f6a588f12d3732fcc199f45bf1e7
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-28 14:20:13 +0530

    navbar created

---
+++ navbar.js
@@ -0,0 +1,9 @@
+const navbar=()=>{
+    let card=`<div id="nav-container>
+    <a href ="index.html">Home</a>
+    <a href ="admin.html">Admin</a>
+    <a href ="books.html">Books</a>
+    </div>`;
+    document.getElementById("nav").innerHTML=card;
+};
+navbar()
