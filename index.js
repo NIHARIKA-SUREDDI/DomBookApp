418f7606219426e2abb1ea7c1aee48a1d1eb4396
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-28 15:53:11 +0530

    adminpage created

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let form =document.getElementById("form")
-form.addEventListener("submit",function(){
+form.addEventListener("submit",function(event){
     event.preventDefault();
      
     let email=form.email.value;
     let password=form.password.value;
 
-     email="admin@empher.com";
-     password="empher@123";
+     
 
      if (email="admin@empher.com" && password=="empher@123"){
-        alert("logged in as Admin"),
-        window.location.href="admin.html"
+        
+        window.location.href="admin.html";
+        alert("logged in as Admin")
+     }
+
+     if (email="user@empher.com"&& password=="user@123"){
+      window.location.href="books.html";
+     }
+     else{
+      alert(" user credential are incorrect");
      }
 })
